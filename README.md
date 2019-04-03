2cb89fba382ebbffe2a2481776f428eb6aa2bbd9
person4268 <person4268@person4269.cf>
2019-04-03 00:12:55 -0400

    Basic websocket listening, nothing splix specific

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A sort of library for interacting with splix.io's protocol at a *slightly* highe
 
 This library is NOT meant to be used as an `npm` package, it is intended to be used as a git submodule. 
 
-Currently incomplete, this file will hopefully contain documentation when I feel this is complete enough. 
+Currently very incomplete, this file will hopefully contain documentation when I feel this is complete enough. 
--- protocollib.js
+++ protocollib.js
@@ -1,1 +1,33 @@
 const WebSocketServer = require('websocket');
+
+class Client { //Class just incase for the future
+    constructor(Connection) {
+        this.connection = Connection;
+    }
+}
+
+class Server {
+    constructor(Http) {
+        this.http = http;
+        this.clients = [];
+        this.socketServer = new WebSocketServer({httpServer: this.http}); //Handle websocket connections
+
+
+        this.socketServer.on("request", (request)=>{
+            let connection = request.accept(null, request.origin); //I did it like this and I don't know if it works without request.origin so here it is
+            let client = new Client(connection);
+            this.clients.push(client);
+
+            connection.on("message", (message)=>{
+                if(message.type === "binary") {
+                    //Process message
+                }
+            });
+        });
+    }
+}
+
+
+module.exports = {
+    Server: Server
+}
