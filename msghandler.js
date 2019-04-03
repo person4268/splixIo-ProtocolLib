6af7d3013a513a2a900c084392f38fb11f704a0a
person4268 <person4268@person4269.cf>
2019-04-03 16:41:57 -0400

    Some basic handling

--- msghandler.js
+++ msghandler.js
@@ -1,4 +1,5 @@
 const ids = require("./ids");
+var players = [];
 
 //Adapted from splix because i hate working with bits
 //And I can't understand whats its doing becuase it's minified
@@ -14,7 +15,12 @@ function bytesToInt() {
     }
     return e
 }
-
+/**
+ * Converts an integer to bytes. 
+ * For example, giving 1, 2 would give you [0x00, 0x01] as you want 2 bytes and the number 1
+ * @param {Number} data 
+ * @param {Number} length 
+ */
 function intToBytes(data, length) {
     for (var n = [], a = 0; a < length; a++) {
         var i = 255 & data;
@@ -24,10 +30,54 @@ function intToBytes(data, length) {
 }
 
 
+/**
+ * Gets a key by its value in any given object
+ * @param {Object} object 
+ * @param {*} value 
+ */
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+class Player { 
+    constructor(Client) {
+        this.client = Client;
+    }
+    send(command, data = []) {
+        console.log(`SEND ${getKeyByValue(sendAction, command)} ${data.toString().split(",").join(" ".substr(1, data.length-1))}`); //Need to format data better. Doesn't seem to work correctly
+        let commandBuf = Buffer.alloc(1, command);
+        let dataBuf = Buffer.from(data);
+        let sendBuf = Buffer.concat([commandBuf, dataBuf]);
+        this.respondHandle.sendBytes(sendBuf);
+
+    }
+}
+
+/**
+ * Creates an internal representation of a player. Seperate from the connection. 
+ * @param {} client 
+ */
+function createPlayer(client) {
+    players[client] = new Player(client);
+}
 
 /**
  * Handler for when message gets sent by splix client. 
  */
 module.exports = function(client, message) {
-    c
+    const msgData = message.binaryData;
+    const command = message.binaryData[0]; //First byte is always the command
+    const commandStr = getKeyByValue(ids.recieveAction, command);
+    const data = msgData.slice(1); // Data for said command comes after. 
+
+    if(!players[client]) {
+        createPlayer(client); //Create player if there isn't one
+    }
+    var player = players[client];
+
+    const inspected = require("util").inspect(data);
+    const inspected_cropped = inspected.substring(8, inspected.length-1);
+    console.log(`RECV ${commandStr} ${inspected_cropped}`);
+
+    
 }
